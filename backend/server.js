4ae921deda938612659fc80964cdc25a97998778
Ignacio Alegre <ignaalegre@hotmail.com>
2025-02-08 20:04:25 -0300

    detalle

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/user.routes.js";
 import { v2 as cloudinary } from "cloudinary";
-import postRoutes from "./routes/post.routes.js";
+
+import connectMongoDB from "./db/connectMongoDB.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 
 dotenv.config();
 
