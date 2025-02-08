2cbf01b9b420f0ddef03423d4ee298480432fe80
Ignacio Alegre <ignaalegre@hotmail.com>
2025-02-08 02:14:36 -0300

    post routes y controllers

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
 import { v2 as cloudinary } from "cloudinary";
+import postRoutes from "./routes/post.routes.js";
 
 dotenv.config();
 
@@ -12,9 +13,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 app.use(express.json()); // para parsear el body de las request.
@@ -22,6 +23,7 @@ app.use(express.urlencoded({ extended: true })); // para parsear formdata (urlen
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
